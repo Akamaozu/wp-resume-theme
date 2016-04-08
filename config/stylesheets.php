f78d706171b36fe709109ef55f04dd0a6632541f
akamaozu <uzo@designbymobius.ca>
2016-04-08 21:41:52 +0100

    experimenting with handwriting font

--- config/stylesheets.php
+++ config/stylesheets.php
@@ -12,7 +12,7 @@
     
     array(
 
-      'src' =>  'https://fonts.googleapis.com/css?family=Exo:900',
+      'src' =>  'https://fonts.googleapis.com/css?family=Exo:900|Homemade+Apple',
       'name' => 'google-fonts'
     ),
     
