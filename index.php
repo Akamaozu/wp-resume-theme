c826ba6fd2acde90464f2846d2b75787ab73e898
akamaozu <akamaozu>
2016-04-08 16:53:40 +0100

    FIRST BLOOD
    added the basics to be considered a WP Theme

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html>
+  <head>
+    <title>Uzo Olisemeka's Resume</title>
+    <script src="https://use.fonticons.com/556f9c16.js"></script>
+  </head>
+  <body>
+    <div id="main-header">
+      <div class="about-me">
+        <div class="name">Uzo Olisemeka</div>
+        <div class="title">Full-Stack Developer</div>
+        <div class="intro">
+          <p>I <span class="fa fa-heart"></span> JavaScript.</p>
+          <p>If your projects involve using it on your server (Node.js, Meteor.js, APIs, microservices), in the browser (Web Apps, React.js, Browserify) or as part of your build process, I can be a valuable team member.</p>
+          <p>I <span class="fa fa-heart"></span> WordPress.</p>
+          <p>If you're building themes / plugins from scratch, maintaining or extending existing ones, I'd love to help.</p>
+      </div>
+    </div>
+  </body>
+</html>
