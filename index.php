fe6e263cd00e6b8cdb4d53c07857131188b59e01
akamaozu <uzo@designbymobius.ca>
2016-04-23 14:30:12 -0400

    separated header from about us

--- index.php
+++ index.php
@@ -16,15 +16,16 @@
   </head>
   <body>
     <div id="main-header">
-      <div class="about-me">
-        <div class="name">Uzo Olisemeka</div>
-        <div class="title">Full-Stack Developer</div>
-        <div class="intro">
-          <p>I <span class="fa fa-heart"></span> JavaScript</p>
-          <p class='subtext'>A wealth of experience using it server-side (<span class="emphasis">Node.js</span>, <span class="emphasis">Meteor</span>), in the browser (<span class="emphasis">web apps</span>, <span class="emphasis">React</span>) and for desktop applications (<span class="emphasis">Electron</span>).</p>
-          <p>I <span class="fa fa-heart"></span> WordPress</p>
-          <p class='subtext'>If you're building <span class="emphasis">themes</span> from scratch, maintaining or extending existing ones, I'd love to help.</p>
-        </div>
+      <div class="name">Uzo Olisemeka</div>
+      <div class="title">Full-Stack Developer</div>
+    </div>
+
+    <div id="about-me">
+      <div class="intro">
+        <p>I <span class="fa fa-heart"></span> JavaScript</p>
+        <p class='subtext'>A wealth of experience using it server-side (<span class="emphasis">Node.js</span>, <span class="emphasis">Meteor</span>), in the browser (<span class="emphasis">web apps</span>, <span class="emphasis">React</span>) and for desktop applications (<span class="emphasis">Electron</span>).</p>
+        <p>I <span class="fa fa-heart"></span> WordPress</p>
+        <p class='subtext'>If you're building <span class="emphasis">themes</span> from scratch, maintaining or extending existing ones, I'd love to help.</p>
       </div>
     </div>
 
